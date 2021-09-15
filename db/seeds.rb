3e488219023047d91113a9240b071852d011212e
JoePeterson51 <>
2021-09-14 19:12:11 -0600

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create!(username: "Teaman500@teateatea.com", password_digest: "DrinkMoreTea")
+user2 = User.create!(username: "Teaman500@teateatea.com2", password_digest: "DrinkMoreTea2")
+tea1 = Tea.create!(name: "Green")
+tea2 = Tea.create!(name: "Black")
+tea3 = Tea.create!(name: "Chai")
+subscription1 = Subscription.create!(user_id: user.id, tea_id: tea1.id, status: "Active")
+subscription2 = Subscription.create!(user_id: user.id, tea_id: tea2.id, status: "Cancelled")
+subscription3 = Subscription.create!(user_id: user.id, tea_id: tea3.id, status: "Active")
+subscription4 = Subscription.create!(user_id: user2.id, tea_id: tea3.id, status: "Active")
+subscription5 = Subscription.create!(user_id: user2.id, tea_id: tea1.id, status: "Active")
